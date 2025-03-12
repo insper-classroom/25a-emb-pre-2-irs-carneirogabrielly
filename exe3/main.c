2aec835527fb81781338aff4fcea2e1718e87739
Gabrielly <gabriellycs@al.insper.edu.br>
2025-03-12 11:44:04 -0300

    exe 3

--- exe3/main.c
+++ exe3/main.c
@@ -4,13 +4,17 @@
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
+volatile int FLAG_R;
+volatile int FLAG_G;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
     if (gpio == BTN_PIN_R)
-      printf("fall red\n");
+      // printf("fall red\n");
+      FLAG_R = 1;
     else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
+      // printf("fall green\n");
+      FLAG_G = 1;
   }
 }
 
@@ -33,5 +37,13 @@ int main() {
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if (FLAG_R) {
+      printf("fall red\n");
+      FLAG_R = 0;
+    }
+    if (FLAG_G) {
+      printf("fall green\n");
+      FLAG_G = 0;
+    }
   }
 }
